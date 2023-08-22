f7077125ab946d13a679e39ed48854d1659b12f9
Alish Bista <bistace321@gmail.com>
2023-08-22 20:50:33 +0545

    feat: Signup form module

--- client/models.py
+++ client/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+    medicare_number = models.CharField(max_length=12)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
--- client/views.py
+++ client/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from .models import UserProfile
+
+
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.email = form.cleaned_data.get('email')
+            user.first_name = form.cleaned_data.get('first_name')
+            user.last_name = form.cleaned_data.get('last_name')
+            user.profile.phone = form.cleaned_data.get('phone')
+            user.profile.address = form.cleaned_data.get('address')
+            user.profile.medicare_number = form.cleaned_data.get('medicare_number')
+            user.profile.profile_pic = form.cleaned_data.get('profile_pic')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Redirect to the home page after successful signup
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
